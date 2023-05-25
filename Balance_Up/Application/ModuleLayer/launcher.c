cf95bd16c1d87d2a06a1666134c5535340cecb7e
ssdcfxb <778911093@qq.com>
2023-05-26 03:51:02 +0800

    [feat] 国防科大前代码2.0：将大符自动打弹间隔由0.8s改为1s

--- Balance_Up/Application/ModuleLayer/launcher.c
+++ Balance_Up/Application/ModuleLayer/launcher.c
@@ -627,7 +627,7 @@ void Launcher_GetKeyState(void)
 	else if ((status.autobuff_cmd == auto_buff_on) && (vision_sensor.work_state == DEV_ONLINE) && (status.buff_cmd == big_buff_on) &&\
 			(status.gim_mode == vision) && (vision_sensor.info->is_hit_enable == 1) && (vision_sensor.info->is_find_buff == 1) && (status.lch_state.fric_state == fric_on))
 	{
-		if (++buff_cnt >= 800)
+		if (++buff_cnt >= 1000)
 		{
 			buff_cnt = 0;
 			status.lch_cmd.shoot_cmd = single_shoot;
